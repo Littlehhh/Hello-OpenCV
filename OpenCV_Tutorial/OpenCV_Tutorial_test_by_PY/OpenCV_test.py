3c65b9558b9fbb9984b22e2506e4b81ca91fc8b8
Littlehhh <wanghuichn@yahoo.com>
2017-05-10 15:04:08 +0800

    UPdate

--- OpenCV_Tutorial/OpenCV_Tutorial_test_by_PY/OpenCV_test.py
+++ OpenCV_Tutorial/OpenCV_Tutorial_test_by_PY/OpenCV_test.py
@@ -1,9 +1,29 @@
 import cv2
 import numpy as np
+import matplotlib
+matplotlib.use("Qt5Agg")
+from matplotlib import pyplot as plt
 
+"""
 img = cv2.imread('blurred.png')
 img.shape
-
+img.size
+img.all()
+img.view()
+plt.show()
 cv2.imshow('lena',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
+
+img = cv2.imread('test.jpg')
+#img2 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#b,g,r = cv2.split(img)
+#img2 = cv2.merge([r,g,b])
+
+img2 = img[:,:,::-1]  #numpy ways
+# three ways for BGR2RGB
+img2
+plt.imshow(img2)
+plt.show()
