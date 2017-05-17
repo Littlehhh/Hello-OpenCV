b8c9a4e3493010798aac257d6d97c7cf08c0e2a4
Littlehhh <wanghuichn@yahoo.com>
2017-05-17 11:01:12 +0800

    Update

--- OpenCV_Tutorial/OpenCV_Tutorial_test_by_CPP/OpenCV_Tutorial_test/OpenCV_Tutorial_test.cpp
+++ OpenCV_Tutorial/OpenCV_Tutorial_test_by_CPP/OpenCV_Tutorial_test/OpenCV_Tutorial_test.cpp
@@ -15,7 +15,8 @@ int main()
 	Mat M2(Size(3,5), CV_8U);
 	int sizes[] = {5, 3};//size用于指定Mat各个维度的数目
 	Mat M3(2,sizes, CV_8U);
-	Mat M11(5, 3, CV_8U, Scalar(2, 1));//Scalar为一个可以最多拥有四个元素的标量
+
+	Mat M11(5, 3, CV_8UC1, Scalar(2, 1));//Scalar为一个可以最多拥有四个元素的标量
 	Mat M21(Size(3, 5), CV_8UC2, Scalar(2, 1)); //注意Size的长宽指定为Size(cols,rows)
 	Mat M31(2, sizes, CV_8UC3, Scalar(2, 1, 3));
 	
@@ -28,7 +29,7 @@ int main()
 	M2.convertTo(M10,CV_64F);
 	
 	Mat image = imread("test.jpg");
-	cvtColor(image, image, CV_BGR2RGB);//读进图像的通道需要转换才符合正常RGB
+//	cvtColor(image, image, CV_BGR2RGB);//读进图像的通道需要转换才符合正常RGB
 	std::cout <<   std::endl;
     return 0;
 }
